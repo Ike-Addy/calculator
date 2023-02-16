a083b67b06b867cc371b2f05bff38c2dcbb0b9eb
Ike Addy <ikaddy05@gmail.com>
2023-02-16 05:51:46 -0500

    Created operations function Attempting to link compute values inside containers with the operators

--- scripts.js
+++ scripts.js
@@ -7,23 +7,45 @@ const deleteButton = document.querySelector('[data-delete]');
 const previousOperandContainer = document.querySelector('[data-previous-operand]');
 const currentOperandContainer = document.querySelector('[data-current-operand]');
 
-
-
-//For each number clicked, show the number clicked in the output screen
+//For each number clicked, show the number clicked in the lower output screen
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        currentOperandContainer.append(button.innerText)   
+        currentOperandContainer.append(button.innerText)
     })
 })
 
-/**For each operator clicked, show the operator and also show the numbers before
-before the operators in the topmost output and the numbers after in the bottom output**/
+console.log(operationButtons);
+
+/** Moves the first set of numbers to the upper output panel **/
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
+        operations(currentOperandContainer, previousOperandContainer);
+        //When an operation button is clicked, copy the contents of the lower container to the upper container
         previousOperandContainer.append(currentOperandContainer.innerText);
+        //Adds the operator to the top container
+        previousOperandContainer.append(button.innerText);
+        //Keeps contains in lower container
         currentOperandContainer.append(button.innerText);
+        
+        
+
     })
-})
+});
+
+function operations (a, b) {
+    if (operationButtons === '÷') {
+        return a / b;
+    }
+    if (operationButtons === 'x') {
+        return a * b;
+    } 
+    if (operationButtons === '+') {
+        return a + b;
+    } 
+    if (operationButtons === '-') {
+        return a - b;
+    }
+};
 
 //Clear output screen when AC is clicked
 allClearButton.addEventListener('click', () => {
@@ -36,14 +58,10 @@ deleteButton.addEventListener('click', () => {
     currentOperandContainer.innerText = currentOperandContainer.innerText.slice(0, -1);
 });
 
-// function operation (a, b) {
-//     equalsButton.addEventListener('click', () => {
-//         let toNumbers = parseInt(currentOperandContainer.innerText);
-//         if (operationButtons == x) {
-//             return a * b;
-//         } else if (operationButtons == ÷) {
-//             return a / b;
-//         }
-        
-//     })
-/** sasa **/
+//The Plan
+/** When an operation is clicked, begin computation by shifting the contents + operator in the currentcontainer to the top container
+ * Then, compute if another operator is clicked or if the equal sign is clicked
+ */
+
+
+
