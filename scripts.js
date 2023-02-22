ad3b016a9a0c14163211ac3c184ef4337a8016b3
Ike Addy <ikaddy05@gmail.com>
2023-02-21 20:26:57 -0500

    Empty commit to allow me to work on project from laptop

--- scripts.js
+++ scripts.js
@@ -4,59 +4,78 @@ const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const deleteButton = document.querySelector('[data-delete]');
-const previousOperandContainer = document.querySelector('[data-previous-operand]');
-const currentOperandContainer = document.querySelector('[data-current-operand]');
+const upperContainer = document.querySelector('[data-previous-operand]');
+const lowerContainer = document.querySelector('[data-current-operand]');
 
-//For each number clicked, show the number clicked in the lower output screen
+//Output for numbers and assign the innerText to values
 numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        currentOperandContainer.append(button.innerText)
+    button.addEventListener('click', () => {        
+        lowerContainer.append(button.innerText);
+        button.value = button.innerText;       
+        // console.log(button.innerText);
+        // console.log(button.value);
+        // console.log(lowerContainer.innerText);
     })
 })
 
-console.log(operationButtons);
-
-/** Moves the first set of numbers to the upper output panel **/
+// Output for operators
 operationButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        operations(currentOperandContainer, previousOperandContainer);
-        //When an operation button is clicked, copy the contents of the lower container to the upper container
-        previousOperandContainer.append(currentOperandContainer.innerText);
-        //Adds the operator to the top container
-        previousOperandContainer.append(button.innerText);
-        //Keeps contains in lower container
-        currentOperandContainer.append(button.innerText);
-        
-        
-
+    button.addEventListener('click', () => {        
+        lowerContainer.append(button.innerText);
+        // console.log(button.value);
+        // console.log(button.innerText);
+        // console.log(lowerContainer.innerText);
+        button.disabled = true;
     })
 });
 
-function operations (a, b) {
-    if (operationButtons === '÷') {
-        return a / b;
-    }
-    if (operationButtons === 'x') {
-        return a * b;
-    } 
-    if (operationButtons === '+') {
-        return a + b;
-    } 
-    if (operationButtons === '-') {
-        return a - b;
-    }
-};
+equalsButton.addEventListener('click', () => {
+    console.log(equalsButton.value);
+    console.log(equalsButton.innerText);
 
-//Clear output screen when AC is clicked
-allClearButton.addEventListener('click', () => {
-    currentOperandContainer.innerText = '' ;
-    previousOperandContainer.innerText = '' ;
+    //compute();
+    //upperContainer.append(lowerContainer.innerText);
 })
 
-//Deletes the last number or operator when DEL is clicked
-deleteButton.addEventListener('click', () => {
-    currentOperandContainer.innerText = currentOperandContainer.innerText.slice(0, -1);
+// function compute (a, b) {
+//     if (operationButtons === '÷') {
+//         divide();
+//     }
+//     if (operationButtons === 'x') {
+//         multiply();
+//     } 
+//     if (operationButtons === '+') {
+//         add();
+//     } 
+//     if (operationButtons === '-') {
+//         subtract();
+//     }
+// };
+
+//Clear all button
+allClearButton.addEventListener('click', () => {
+    lowerContainer.innerText = '' ;
+    upperContainer.innerText = '' ;
 });
+
+//Delete a single item
+deleteButton.addEventListener('click', () => {
+    lowerContainer.innerText = lowerContainer.innerText.slice(0, -1);
+});
+
+//Operator functions
+function add (a,b) {
+    return a + b;
+}
+function subtract (a,b) {
+    return a - b;
+}
+function multiply (a,b) {
+    return a * b;
+}
+function divide (a,b) {
+    return a / b;
+}
 
 //The Plan
 /** When an operation is clicked, begin computation by shifting the contents + operator in the currentcontainer to the top container
