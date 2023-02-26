6acc8549bddcb5589e3afd8d07ca963c431b7c1a
Ike Addy <ikaddy05@gmail.com>
2023-02-26 16:40:46 -0500

    Added calculate function for operands Basic calculation with equal symbol works as expected Missing feature to allow user to string together several operations Missing feature to prevent user from using more than 1 decimal in a single value

--- scripts.js
+++ scripts.js
@@ -1,90 +1,96 @@
-//Use data sets for numbers
+//Store html components in JS
 const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
+const operator = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const deleteButton = document.querySelector('[data-delete]');
-const upperContainer = document.querySelector('[data-previous-operand]');
-const lowerContainer = document.querySelector('[data-current-operand]');
+const previousScreen = document.querySelector('[data-previous-screen]');
+const currentScreen = document.querySelector('[data-current-screen]');
 
-//Output for numbers and assign the innerText to values
-function numberbtns () {
-    numberButtons.forEach(button => {
-        button.addEventListener('click', () => { 
-                
-            lowerContainer.append(button.innerText);
-                
-            // console.log(button.innerText);
-            console.log(button.value);
-            // console.log(lowerContainer.innerText);
-        })
+let operatorValue = '';
+let currentValue = '';
+let previousValue = '';
+
+//Output for numbers and assign the textContent to values (first)
+numberButtons.forEach(button => {
+    button.addEventListener('click', (e) => { 
+       handleNumber(e.target.textContent) 
+       currentScreen.textContent = currentValue;
     })
-    return button.value = button.innerText;
+})
+
+function handleNumber(num){
+    currentValue += num;
 }
-numberbtns();
-// Output for operators
-operationButtons.forEach(button => {
-    button.addEventListener('click', () => {        
-        lowerContainer.append(button.innerText);
-        // console.log(button.value);
-        // console.log(button.innerText);
-        // console.log(lowerContainer.innerText);
-        //button.disabled = true;
+
+// Output for operators  (second)
+operator.forEach(button => {
+    button.addEventListener('click', (e) => {        
+        handleOperator(e.target.textContent)
+        currentScreen.textContent = currentValue; //Should be empty
+        previousScreen.textContent = previousValue + " " + operatorValue;
     })
 });
 
-equalsButton.addEventListener('click', () => {
-    console.log(equalsButton.value);
-    console.log(equalsButton.innerText);
+function handleOperator(op) {
+    operatorValue = op;
+    previousValue = currentValue;
+    currentValue = '';
+}
 
-    //compute();
-    //upperContainer.append(lowerContainer.innerText);
+
+// Calculates previousValue and currentValue when equal sign is clicked (third)
+equalsButton.addEventListener('click', () => {
+    calculate();
+    currentValue = previousValue;
+    currentScreen.textContent = currentValue;
+    previousScreen.textContent = ''; 
 })
 
-// function compute (a, b) {
-//     if (operationButtons === '÷') {
-//         divide();
-//     }
-//     if (operationButtons === 'x') {
-//         multiply();
-//     } 
-//     if (operationButtons === '+') {
-//         add();
-//     } 
-//     if (operationButtons === '-') {
-//         subtract();
-//     }
-// };
+
+function calculate () {
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+ 
+    if (operatorValue === '÷') {
+        previousValue /= currentValue;
+    } else if(operatorValue === 'x') {
+        previousValue *= currentValue;
+    } else if(operatorValue === '+' ) {
+        previousValue += currentValue;
+    } else {
+        previousValue -= currentValue;
+    };
+}
 
 //Clear all button
 allClearButton.addEventListener('click', () => {
-    lowerContainer.innerText = '' ;
-    upperContainer.innerText = '' ;
+    currentScreen.textContent = '' ;
+    previousScreen.textContent = '' ;
+    operatorValue = '';
+    currentValue = '';
+    previousValue = '';
 });
 
 //Delete a single item
 deleteButton.addEventListener('click', () => {
-    lowerContainer.innerText = lowerContainer.innerText.slice(0, -1);
+    currentScreen.textContent = currentScreen.textContent.slice(0, -1);
+    currentValue = currentScreen.textContent;
 });
 
-//Operator functions
-function add (a,b) {
-    return a + b;
-}
-function subtract (a,b) {
-    return a - b;
-}
-function multiply (a,b) {
-    return a * b;
-}
-function divide (a,b) {
-    return a / b;
-}
-
-//The Plan
-/** When an operation is clicked, begin computation by shifting the contents + operator in the currentcontainer to the top container
- * Then, compute if another operator is clicked or if the equal sign is clicked
+/** 1) User inputs a number ++
+ * 2) Store this number in the currentValue variable ++
+ *      >> Numbers are currently stored as strings so i need to turn them into numbers
+ * 3) User selects an operator
+ *      >> When the user selects an operator, move the current number as well as the selected operator into the top(previous screen)++
+ *      >> Clear the currentScreen for a new value to be stored ++
+ * 4) Calcute inputs in previousScreen and currentScreen
+ *      >> Create a function to calculate ++
+ *              && Make sure the function accounts for unique symbols and translates it to mathematical symbols ++
+ * 5) Clear the numbers in the previous screen and move the total value to the current screen++
+ * 
+ * 
+ *  TO DO
+ * >> Allow calculator to string calculation without the equal sign
+ * >> Prevent calculator from using more than 1 decimal point at a time
  */
-
-
-
