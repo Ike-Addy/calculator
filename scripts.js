c351e79258899cae119ab93965f0c30680872b25
Ike Addy <ia@nacua.org>
2023-12-21 12:15:30 -0500

    Changed element names in html Removed computed display Changed variable names in javascript Created 3 variables to store various values Created functions to handle operand and operator displays Added feature to turn strings to numbers
    Next:
        Work on compute function

--- scripts.js
+++ scripts.js
@@ -1,23 +1,39 @@
-const operands = document.querySelectorAll('.operand');
-const operators = document.querySelectorAll('.operator');
-const compute = document.querySelector('#compute');
+const operands = document.querySelectorAll('.operands');
+const operators = document.querySelectorAll('.operators');
+const equalsTo = document.querySelector('#equals-to');
 const clearAll = document.querySelector('#clear-all');
-const currentDisplay = document.querySelector('.current-display');
-const computedDisplay = document.querySelector('.computed-display');
+const display = document.querySelector('#display');
+
+let storeFirstOperand = null;
+let storeCurrentOperand = null;
+let storeOperator = null;
 
 
+// Store & display operand and operator 
+function displayOperandClicked (e) {
+    display.textContent += e.target.textContent;
+}
+function displayOperatorClicked (e) {
+    storeFirstOperand = display.textContent
+    storeFirstOperand = parseFloat(storeFirstOperand);
+    storeOperator = e.target;
+    display.textContent += e.target.textContent;
+}
 operands.forEach(operand => {
-    operand.addEventListener('click', (e) => {
-        alert(e.target.innerText);
-    })
+    operand.addEventListener('click', displayOperandClicked);
 })
-
 operators.forEach(operator => {
-    operator.addEventListener('click', (e) => {
-        alert(e.target.innerText);
-    })
+    operator.addEventListener('click', displayOperatorClicked);
 })
+// End
 
-compute.addEventListener('click', () => {
-    alert(compute.innerText);
-})
+// Function for computation & handleClick to run computation
+function compute () {
+
+}
+
+
+
+
+
+
