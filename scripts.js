741d670a74beb9b30edb407195ba824a8d5a01a0
Ike Addy <ikaddy05@gmail.com>
2023-12-28 23:36:42 -0500

    Empty commit

--- scripts.js
+++ scripts.js
@@ -1,101 +1,96 @@
-const operands = document.querySelectorAll('.operands');
-const operators = document.querySelectorAll('.operators');
-const equalsTo = document.querySelector('#equals-to');
-const clearAll = document.querySelector('#clear-all');
-const display = document.querySelector('#display');
+//Store html components in JS
+const numberButtons = document.querySelectorAll('[data-number]');
+const operator = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const previousScreen = document.querySelector('[data-previous-screen]');
+const currentScreen = document.querySelector('[data-current-screen]');
 
-let initialOperand = null;
-let currentOperand = null;
-let storeSolution = null;
-let storeOperator = null;
+let operatorValue = '';
+let currentValue = '';
+let previousValue = '';
 
-// Store & display operands and operators && turn string to number
-const displayBtnClicked = function(e) {
-    display.textContent += e.target.textContent;
-};
-
-const handleOperator = function (e) {
-    // Store first operator for later use in compute function
-    initialOperand = display.textContent;
-    // Get the clicked operator
-    storeOperator = e.target.textContent;
-
-    // Check if the display is empty, do not allow an operator input
-    if (display.textContent === '') {
-        return;
-    }
-
-    // Check if the display has an operator, do not allow a second operator
-    const availableOperators = ['+', '-', 'x', '/'];
-    const lastChar = display.textContent.charAt(display.textContent.length - 1);
-
-    if (availableOperators.includes(lastChar)) {
-        // If the last character is an operator, do not allow a second operator
-        return;
-    }
-
-    // Append the new operator to the display
-    display.textContent += storeOperator;
-};
-
-operands.forEach(operand => {
-    operand.addEventListener('click', displayBtnClicked);
+//Output for numbers and assign the textContent to values (first)
+numberButtons.forEach(button => {
+    button.addEventListener('click', (e) => { 
+       handleNumber(e.target.textContent) 
+       currentScreen.textContent = currentValue;
+    })
 })
-operators.forEach(operator => {
-    operator.addEventListener('click', handleOperator);
-})
-// End
 
-clearAll.addEventListener('click', () => {
-    display.textContent = '';
-    initialOperand = null;
-    currentOperand = null;
-    storeSolution = null; 
-    storeOperator = null;
+function handleNumber(num){
+    currentValue += num;
+}
+
+// Output for operators  (second)
+operator.forEach(button => {
+    button.addEventListener('click', (e) => {        
+        handleOperator(e.target.textContent)
+        currentScreen.textContent = currentValue; //Should be empty
+        previousScreen.textContent = previousValue + " " + operatorValue;
+    })
 });
 
-// Function for computations & handleClick & operator transformation from string to symbol
-const compute = function () {
-    // Check if the display is empty
-    if (display.textContent === '') {
-        return;
-    }
+function handleOperator(op) {
+    operatorValue = op;
+    previousValue = currentValue;
+    currentValue = '';
+}
 
-    // Split the expression based on operators
-    const expression = display.textContent.split(/([+\-x/])/);
 
-    // Initialize variables for computation
-    let result = parseFloat(expression[0]); // Initialize with the first operand
+// Calculates previousValue and currentValue when equal sign is clicked (third)
+equalsButton.addEventListener('click', () => {
+    calculate();
+    currentValue = previousValue;
+    currentScreen.textContent = currentValue;
+    previousScreen.textContent = ''; 
+})
 
-    // Loop through the expression parts
-    for (let i = 1; i < expression.length; i += 2) {
-        const operator = expression[i];
-        const operand = parseFloat(expression[i + 1]);
 
-        // Perform the operation based on the operator
-        switch (operator) {
-            case '+':
-                result += operand;
-                break;
-            case '-':
-                result -= operand;
-                break;
-            case 'x':
-                result *= operand;
-                break;
-            case '/':
-                result /= operand;
-                break;
-            default:
-                break;
-        }
-    }
+function calculate () {
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+ 
+    if (operatorValue === '÷') {
+        previousValue /= currentValue;
+    } else if(operatorValue === 'x') {
+        previousValue *= currentValue;
+    } else if(operatorValue === '+' ) {
+        previousValue += currentValue;
+    } else {
+        previousValue -= currentValue;
+    };
+}
 
-    // Update the display with the result
-    display.textContent = result;
-    
-};
+//Clear all button
+allClearButton.addEventListener('click', () => {
+    currentScreen.textContent = '' ;
+    previousScreen.textContent = '' ;
+    operatorValue = '';
+    currentValue = '';
+    previousValue = '';
+});
 
-equalsTo.addEventListener('click', compute);
-// End
+//Delete a single item
+deleteButton.addEventListener('click', () => {
+    currentScreen.textContent = currentScreen.textContent.slice(0, -1);
+    currentValue = currentScreen.textContent;
+});
 
+/** 1) User inputs a number ++
+ * 2) Store this number in the currentValue variable ++
+ *      >> Numbers are currently stored as strings so i need to turn them into numbers
+ * 3) User selects an operator
+ *      >> When the user selects an operator, move the current number as well as the selected operator into the top(previous screen)++
+ *      >> Clear the currentScreen for a new value to be stored ++
+ * 4) Calcute inputs in previousScreen and currentScreen
+ *      >> Create a function to calculate ++
+ *              && Make sure the function accounts for unique symbols and translates it to mathematical symbols ++
+ * 5) Clear the numbers in the previous screen and move the total value to the current screen++
+ * 
+ * 
+ *  TO DO
+ * >> Allow calculator to string calculation without the equal sign
+ * >> Prevent calculator from using more than 1 decimal point at a time
+ */
